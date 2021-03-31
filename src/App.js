846cd271cfa0dc70715e3847c581500cd3aeeeab
Iryna Audzei <avirene@live.com>
2021-03-31 15:59:10 +0100

    Added the folder structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from "./screens/login";
+import { useEffect } from "react";
+import { LoginData } from "./data/LoginData";
+import "semantic-ui-css/semantic.min.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+import Profile from "./screens/profile";
 
-function App() {
+const isLogin = () => {
+  if (localStorage.getItem("loggedInUser")) return true;
+  return false;
+};
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  // Show the component only when the user is logged in
+  // Otherwise, redirect the user to /signin page
+  <Route
+    {...rest}
+    render={(props) =>
+      isLogin() ? <Component {...props} /> : <Redirect to="/" />
+    }
+  />
+);
+
+export default function App() {
+  useEffect(() => {
+    localStorage.setItem("users", JSON.stringify(LoginData));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <PrivateRoute component={Profile} path="/profile" />
+        <Route path="/">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
+    // <div className="App">
+    //   <div style={{ maxWidth: "300px", margin: "0 auto", padding: "30px 0" }}>
+    //     <Login />
+    //   </div>
+    // </div>
   );
 }
-
-export default App;
---
+++ src/screens/login/login.js
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import md5 from "md5";
+import { Form, Button } from "semantic-ui-react";
+import { useHistory } from "react-router-dom";
+
+const Login = () => {
+  const [form, setForm] = useState({
+    username: "",
+    password: ""
+  });
+
+  const [err, setError] = useState("");
+
+  const handleOnChange = ({ target: { name, value } }) => {
+    setForm({ ...form, [name]: value });
+  };
+
+  const history = useHistory();
+
+  const handleLoginSubmit = (_) => {
+    setError("");
+    const submission = {
+      ...form,
+      password: md5(form.password)
+    };
+    const users = JSON.parse(localStorage.getItem("users"));
+    const user = users.find((item) => {
+      return item.username === submission.username;
+    });
+    const loginSuccessful = user
+      ? user.password === submission.password
+        ? true
+        : false
+      : false;
+    if (loginSuccessful) {
+      localStorage.setItem("loggedInUser", user.username);
+      history.push("/profile");
+    } else {
+      setError("Login unsuccessful");
+    }
+  };
+
+  console.log("Login unsuccessful:", err);
+
+  return (
+    <>
+      <h1>Login</h1>
+      <Form onSubmit={handleLoginSubmit}>
+        <Form.Field>
+          <label>Username</label>
+          <input
+            name="username"
+            placeholder="Username"
+            type="text"
+            onChange={handleOnChange}
+            value={form.username}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Password</label>
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            onChange={handleOnChange}
+            value={form.password}
+          />
+        </Form.Field>
+        <Button type="submit">Submit</Button>
+
+        {err && <h2>error</h2>}
+      </Form>
+    </>
+  );
+};
+
+export default Login;
